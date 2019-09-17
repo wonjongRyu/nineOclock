48ba9d875a48add924bd62da6cb4e1a11f12ab23
wonjongRyu <wonjong4773@gmail.com>
2019-09-17 20:52:04 +0900

    controller!!

---
+++ controllers/userControllers.js
@@ -0,0 +1,9 @@
+export const home = (req, res) => res.send("Home");
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Logout");
+export const search = (req, res) => res.send("Search");
+export const users = (req, res) => res.send("Users");
+export const userDetail = (req, res) => res.send("User Detail");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
