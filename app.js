2cf162d55ccb7a02b9d55be3a61495269df1df64
wonjongRyu <wonjong4773@gmail.com>
2019-09-17 20:24:52 +0900

    router_so_cool

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Welcome Home!");
-
-const handleProfile = (req, res) => res.send("This is my Profile!");
 
 app.use(helmet());
 app.use(morgan("dev"));
@@ -17,10 +15,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
 
 export default app;
