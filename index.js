079ce770ea2462f8d7c0e3ddba8af2f654687768
wonjongRyu <wonjong4773@gmail.com>
2019-09-17 17:56:02 +0900

    babel and nodemon

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require("express"); // node module 속의 express를 가져온다.
+import express from "express"; // node module 속의 express를 가져온다.
 const app = express(); // express를 실행해서 app에 담는다.
 
 const PORT = 4000; // PORT 번호
 
-// CallBack 함수
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+const handleHome = (req, res) => res.send("Welcome Home!");
+
+const handleProfile = (req, res) => res.send("This is my Profile!");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 // 누군가 http://localhost:4000 으로 접속하면 CallBack함수 실행
 app.listen(PORT, handleListening);
