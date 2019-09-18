b3c100bd639ed738ab74e7c4a8a0ec9e7d8ce18f
wonjongRyu <wonjong4773@gmail.com>
2019-09-18 20:41:36 +0900

    update search!

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -12,5 +12,5 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, books });
 };
