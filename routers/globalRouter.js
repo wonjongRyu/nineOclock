d7c62b8a7942b18470271b582e28f8f131e68771
wonjongRyu <wonjong4773@gmail.com>
2019-09-18 21:05:50 +0900

    JoinIsDevidedTo_Get/Post

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { home, join, login, search } from "../controllers/globalController";
+import {
+  home,
+  getJoin,
+  postJoin,
+  login,
+  search
+} from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.login, login);
+
 globalRouter.get(routes.search, search);
 
 export default globalRouter;
